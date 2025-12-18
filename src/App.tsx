501258421c92cb9b8c956cf06360307594eb6ef6
KozmaSzabolcsAndras <koszabi7@gmail.com>
2025-12-18 19:02:22 +0100

    teszt

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline text-red-500">Teszt</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
